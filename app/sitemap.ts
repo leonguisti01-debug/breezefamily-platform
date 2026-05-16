e3bf0a23226bb74bda692169d08a52ce2b105d63
leon guisti <leonguisti01@gmail.com>
2026-05-16 13:36:18 +0200

    Added manual sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.breezefamily.co.za",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.breezefamily.co.za/kids-edition",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.breezefamily.co.za/season-2-finale",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.breezefamily.co.za/fan-favorite-judge",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://www.breezefamily.co.za/admin",
+      lastModified: new Date(),
+    },
+  ];
+}
